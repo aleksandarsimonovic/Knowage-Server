6d79282e877dca3413b38aa2e7bd96f98ec470c3
Darko Bulatovic <darko.bulatovic@eng.it>
2021-05-21 17:21:20 +0200

    Configuration management initial commit

---
+++ knowage-vue/src/modules/managers/configurationManagement/ConfigurationManagement.d.ts
@@ -0,0 +1,10 @@
+export interface iConfiguration {
+    id: number | null;
+    label: string;
+    name: string;
+    description: string;
+    active: boolean | null;
+    valueCheck: string;
+    category: string;
+  }
+  
