4fbf1e4a0e3f2f8849f3823989f68382e7929bf5
BojanSovticEngIT <Bojan.Sovtic@eng.it>
2021-05-26 14:01:40 +0200

    Added list-box.

--- knowage-vue/src/modules/managers/managers.routes.js
+++ knowage-vue/src/modules/managers/managers.routes.js
@@ -1,16 +1,16 @@
 import galleryManagementRoutes from '@/modules/managers/galleryManagement/GalleryManagement.routes.js'
-import configurationManagementRoutes from '@/modules/managers/configurationManagement/ConfigurationManagement.routes.js';
-import usersManagementRoutes from '@/modules/managers/usersManagement/UsersManagement.routes.js';
-import domainsManagementRoutes from '@/modules/managers/domainsManagement/DomainsManagement.routes.js';
-import metadataRoutes from '@/modules/managers/metadata/Metadata.routes.js';
+import configurationManagementRoutes from '@/modules/managers/configurationManagement/ConfigurationManagement.routes.js'
+import usersManagementRoutes from '@/modules/managers/usersManagement/UsersManagement.routes.js'
+import domainsManagementRoutes from '@/modules/managers/domainsManagement/DomainsManagement.routes.js'
+import metadataManagementRoutes from '@/modules/managers/metadataManagement/MetadataManagement.routes.js'
 
-const baseRoutes = [];
+const baseRoutes = []
 
 const routes = baseRoutes
-.concat(galleryManagementRoutes)
-.concat(configurationManagementRoutes)
-.concat(usersManagementRoutes)
-.concat(domainsManagementRoutes)
-.concat(metadataRoutes);
+    .concat(galleryManagementRoutes)
+    .concat(configurationManagementRoutes)
+    .concat(usersManagementRoutes)
+    .concat(domainsManagementRoutes)
+    .concat(metadataManagementRoutes)
 
-export default routes;
+export default routes
